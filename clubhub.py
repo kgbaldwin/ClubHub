b067c19f78d80ed8d0f7f60711367f6ba451c837
Priya Naphade <priyanaphade31@gmail.com>
2022-10-12 18:02:40 -0400

    initial attempt

---
+++ clubhub.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------
+# clubhub.py
+# Authors: Kevin Kim, Priya Naphade, Katie Baldwin, Lance Yoder
+#-------------------------------------------------------------------
+
+import time
+import flask
+import database
+
+#-------------------------------------------------------------------
+
+app= flask.Flask(__name__)
+
+#-------------------------------------------------------------------
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    html_code = flask.render_template('index.html')
+    response = flask.make_response(html_code)
+    return response
+
+@app.route('/searchform', method=['GET'])
+def searchform():
+    html_code = flask.render_template('searchform.html')
+    response = flask.make_response(html_code)
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run()
