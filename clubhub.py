fb210d1cc745786ebe2aba83161ae70e28c4d5e0
Priya Naphade <priyanaphade31@gmail.com>
2022-10-30 22:46:11 -0400

    database modularization, more error handling

--- clubhub.py
+++ clubhub.py
@@ -7,20 +7,12 @@
 
 import flask
 import psycopg2
-#import database
+import database
 import urllib.parse as up
-
-
-database_url = "postgres://avgqxjcj:lg3PfhN5-G_5-KH1XleCGMAJgHkZfcN1@peanut.db.elephantsql.com/avgqxjcj"
-
 
 #-------------------------------------------------------------------
 
 app = flask.Flask(__name__)
-
-
-
-#-------------------------------------------------------------------
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -43,38 +35,22 @@ def searchresults():
     tags = flask.request.args.getlist('tags')
     print("cq: ", clubquery)
     print("tags: ", tags)
-    args = [clubquery] + tags
-    print("args: ", args)
-    try:
-        with psycopg2.connect(database_url) as conn:
-            with conn.cursor() as cur:
 
-                script = "select clubname from clubs WHERE clubname ILIKE %s "
-                if tags:
-                    script += "AND clubs.clubid=ANY(SELECT clubid from tags where tag=%s "
-                    for tag in tags[1:]:
-                        print("added a tag: ", tag)
-                        script += "or tag=%s "
-                    script +=")"
+    clubs = database.get_clubs(clubquery, tags)
 
-                #script = "select clubname from clubs where clubname ILIKE %s AND clubs.clubid=ANY(SELECT clubid from tags where tag=%s or tag=%s)"
-                #print("script: ", script)
-                cur.execute(script, args)
+    if clubs == "server":
+        print("hello there")
+        html_code = flask.render_template('error.html', error="server")
+        response = flask.make_response(html_code)
+        return response
 
-                retval = cur.fetchall()
-                #print("retval: ")
-                #print(retval)
-
-    except Exception as ex:
-        print(ex)
-
-    html_code = flask.render_template('searchresults.html', results=retval)
+    html_code = flask.render_template('searchresults.html', results=clubs)
     response = flask.make_response(html_code)
     return response
 
 @app.errorhandler(404)
 def page_not_found(e):
-    html_code = flask.render_template('404.html')
+    html_code = flask.render_template('error.html', error="404")
     response = flask.make_response(html_code)
     return response
 
---
+++ database.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------
+# database.py
+# Authors: Kevin Kim, Priya Naphade, Katie Baldwin, Lance Yoder
+#-------------------------------------------------------------------
+
+import flask
+import psycopg2
+
+database_url = "postgres://avgqxjcj:lg3PfhN5-G_5-KH1XleCGMAJgHkZfcN1@peanut.db.elephantsql.com/avgqxjcj"
+
+def get_clubs(clubquery, tags):
+
+    args = [clubquery] + tags
+    print("args: ", args)
+
+
+    try:
+        with psycopg2.connect(database_url) as conn:
+
+            with conn.cursor() as cur:
+
+                script = "select clubname from clubs WHERE clubname ILIKE %s "
+                if tags:
+                    script += "AND clubs.clubid=ANY(SELECT clubid from tags where tag=%s "
+                    for tag in tags[1:]:
+                        print("added a tag: ", tag)
+                        script += "or tag=%s "
+                        script +=")"
+
+                cur.execute(script, args)
+
+                row = cur.fetchone()
+                clubs = []
+                while row is not None:
+                    clubs.append(row)
+                    row = cur.fetchone()
+
+                return clubs
+
+    except Exception as ex:
+        print(ex)
+        return "server"
