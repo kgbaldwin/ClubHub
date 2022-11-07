f53f2c3d9c537145368c92f2854a222b08914134
Priya Naphade <priyanaphade31@gmail.com>
2022-11-07 09:28:14 -0500

    CAS - secret key must be a string

--- clubhub.py
+++ clubhub.py
@@ -14,7 +14,7 @@ import auth
 # -------------------------------------------------------------------
 
 app = flask.Flask(__name__, template_folder='static/templates')
-os.environ['APP_SECRET_KEY'] = 12345 # not very secret???
+os.environ['APP_SECRET_KEY'] = "hello" # not very secret???
 app.secret_key = os.environ['APP_SECRET_KEY']
 
 # -------------------------------------------------------------------
