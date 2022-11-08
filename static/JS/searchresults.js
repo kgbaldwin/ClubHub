ea6a3d2203bfe92209d03e9d1db707adaf05b50a
Kevin Kim <sejk@princeton.edu>
2022-11-08 12:25:47 -0500

    bubbles

--- static/JS/searchresults.js
+++ static/JS/searchresults.js
@@ -3,7 +3,7 @@
 // https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
 //document.querySelector('input[name="clubname"]:checked').value;
 
-function changeInfo() {
+function changeInfo(id) {
 
     //document.getElementById("clubinfo").innerHTML = getInfo(selected_id)
 
@@ -22,8 +22,29 @@ function changeInfo() {
     document.getElementById("clubverified").innerHTML = info[5];
     document.getElementById("clublastup").innerHTML = info[6];
     document.getElementById("clubimlink").innerHTML = info[7];
-    document.getElementById("clubimage").src = 'https://images.squarespace-cdn.com/content/v1/57b50c7559cc680955b06c27/1502749418326-AIJQ3QYSG9NOGDQ2DHRR/Full+horizontal+Logo+transparent.png?format=1500w';
+    document.getElementById("clubimage").src = 'https://i0.wp.com/statisticsbyjim.com/wp-content/uploads/2020/09/association-152746_640.png?resize=300%2C300&ssl=1';
+
+    const previous = document.getElementsByClassName("search-results-card-selected");
+    for (let i = 0; i < previous.length; i++){
+        previous[i].classList.remove('border', 'search-results-card-selected');
+    }
+    document.getElementById("card_"+clubid).classList.add('search-results-card-selected','border','border-warning','border-3');
 });
+}
+
+function getTitle(id) {
+
+    //document.getElementById("clubinfo").innerHTML = getInfo(selected_id)
+
+    clubid = document.querySelector('input[name="clubname"]:checked').value
+    // alert("selected element " + clubid)
+
+    fetch("/get_info?clubid="+clubid)
+    .then((response) => response.text())
+    .then((text) => {
+    var info = text.split("\n");
+    });
+    return info[1];
 }
 
 
